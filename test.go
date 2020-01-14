dca1ea4393528f4c1815f34251ffb9e81ab69a89
root <root@localhost.localdomain>
2020-01-14 09:54:32 +0900

    test.goにmap versionとmanual versionを追加した

--- test.go
+++ test.go
@@ -19,11 +19,12 @@ func main() {
   }
 
   api.SetApp(router)
-  log.Fatal(http.ListenAndServe(":8080", api.MakeHandler()))
+  log.Fatal(http.ListenAndServe(":1234", api.MakeHandler()))
 }
 
 func getVersion(w rest.ResponseWriter, r *rest.Request) {
   w.WriteJson(map[string]string{
+    "version": "0.2.1",
     "map version": "0.1.2",
     "manual version": "0.1.1",
   })
