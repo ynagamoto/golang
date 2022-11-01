f01c9daf1baf8c33e80cad1ea866953d376f36fe
munvei <roots19981108@gmail.com>
2022-11-01 13:23:19 +0900

    add leedcode No2 template

---
+++ leetcode/AddTwoNumbers.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "fmt"
+)
+
+// Definition for singly-linked list.
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+  num := 0
+  if len(l1) >= len(l2) {
+    num = len(l1)
+  } else {
+    num = len(l2)
+  }
+}
+
+func main() {
+
+}
