ebe231c921296bb52c383ca4f5b375ec1c4505af
munvei <roots19981108@gmail.com>
2022-11-01 22:33:15 +0900

    complete leetcode No2

--- leetcode/AddTwoNumbers.go
+++ leetcode/AddTwoNumbers.go
@@ -1,24 +1,35 @@
-package main
-
-import (
-  "fmt"
-)
-
-// Definition for singly-linked list.
-type ListNode struct {
-    Val int
-    Next *ListNode
-}
-
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-  num := 0
-  if len(l1) >= len(l2) {
-    num = len(l1)
-  } else {
-    num = len(l2)
-  }
-}
-
-func main() {
-
+	resNode := ListNode{0, nil}
+	prevNode := &resNode
+	tmpNode := ListNode{0, nil}
+	var currNode *ListNode
+	carry := 0
+	for {        
+			sum := l1.Val + l2.Val + carry
+			curr := sum % 10
+			carry = sum / 10
+			
+			currNode = &ListNode{curr, nil}
+			prevNode.Next = currNode
+			prevNode = currNode
+			
+			flag := 0
+			if l1.Next == nil {
+					l1 = &tmpNode
+					flag++
+			} else {
+					l1 = l1.Next
+			}
+			if l2.Next == nil {
+					l2 = &tmpNode
+					flag++
+			} else {
+					l2 = l2.Next
+			}
+			
+			if flag == 2 && carry == 0 {
+					break
+			}
+	}
+	return resNode.Next  
 }
